704abf967f0bd7623440041b3754c03bbf2b0687
Your Name <you@example.com>
2020-04-24 17:20:18 +0800

    add update_desktop_image

--- spider.py
+++ spider.py
@@ -4,11 +4,13 @@ import urllib
 import requests
 import time
 from datetime import datetime
-from bs4 import BeautifulSoup  # Beautiful Soup是一个可以从HTML或XML文件中提取结构化数据的Python库
+from bs4 import BeautifulSoup 
 import lxml
+import win32api
+import win32gui
+import win32con
 
 def get_img_url(html):
-    # reg = r'src="(.*?jpg)"'
     soup = BeautifulSoup(html, 'lxml')
     taglist = soup.find_all(name='img')
     img_list = []
@@ -39,32 +41,55 @@ def download_image(img_list, title_list, file_dir):
     opener=urllib.request.build_opener()
     opener.addheaders=[("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")]
     i = 0
+    image_path_list = []
     for img_url in img_list:
         image_name = title_list[i].get_text() 
         image_name = re.sub(r'\(.*\)', '', image_name)
         image_name = image_name + '.jpg'
         image_name= image_name.replace(' ', '')
-        work_path=os.path.join(file_dir, image_name)
-        print('Downloading->>>' + work_path)
+        image_path=os.path.join(file_dir, image_name)
+        image_path_list.append(image_path)
+        print('Downloading->>>' + image_path)
         urllib.request.install_opener(opener)
-        urllib.request.urlretrieve(img_url, work_path)
+        urllib.request.urlretrieve(img_url, image_path)
         time.sleep(1)
         i += 1
+    return image_path_list
+
+def update_desktop_image(image_path):
+    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, image_path)
+
 def main():
-    # html = getHtml('https://bing.ioliu.cn/ranking') 
-    html = getHtml('https://bing.ioliu.cn/') 
-    # print(html)
-    img_list = get_img_url(html)
-    title_list = get_img_title(html)
+    try:
+        html = getHtml('https://bing.ioliu.cn/ranking')
+        # html = getHtml('https://bing.ioliu.cn/') 
+        # print(html)
+    except:
+        print('get html failed!')
+        return
+    try:
+        img_list = get_img_url(html)
+    except:
+        print('get img_list failed!')
+        return
+    try:
+        title_list = get_img_title(html)
+    except:
+        print('get title_list failed!')
+        return
+
     image_dir = os.path.abspath('.') + '\\image\\' + datetime.now().strftime('%Y%m%d')
     if os.path.exists(image_dir) is False:
         os.makedirs(image_dir)
-    file_dir = image_dir
-    
-    download_image(img_list, title_list, file_dir)
         
+    try:
+        image_path_list = download_image(img_list, title_list, image_dir)
+    except:
+        print('Downloading->>> ERROR!')
+        return    
     print ('Downloading->>> Finish!!')
 
+    update_desktop_image(image_path_list[3])
 
 if __name__ == '__main__':
     main()
